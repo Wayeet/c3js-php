b2be4ab05b77df70a2fd907e1bab512e47f4dfe2
Wayeet <kontakt@bjoern-roos.com>
2024-09-03 20:51:58 +0200

    BarChart commented and prepared for realease

--- BarChart.php
+++ BarChart.php
@@ -1,28 +1,38 @@
 <?php
+/**
+ * Represents a BarChart (https://c3js.org/samples/chart_bar.html)
+ */
 class BarChart extends UIChart {
 
     public string $id;
     public array $data;
     public bool $showToolTip;
 
+    /**
+     * @param string $id The HTML "id"-Tag
+     * @param array $data Data in the format shown in chart.data.columns -> https://c3js.org/samples/chart_bar.html.
+     * @param bool $showToolTip Whether to show a tooltip on the chart
+     */
     public function __construct(string $id, array $data, bool $showToolTip = false)
     {
         $this->id = $id;
         $this->showToolTip = $showToolTip;
-
-        //FORMAT von $data
-        // $data = [
-        //     ['data1', 0,0,0,0,0,0,0],
-        //     ['data2', 0,0,0,0,0,0,0]
-        // ];
         $this->data = $data;
     }
 
-    public function preRender(){
+    /**
+     * Renders the charts wrapper-element
+     * @return void
+     */
+    public function preRender(): void{
         echo '<div id="'.$this->id.'"></div>';
     }
     
-    public function render() {
+    /**
+     * Render the chart to the wrapper
+     * @return void
+     */
+    public function render(): void {
         $jsonString = json_encode($this->data);
         $jsonShowString = json_encode($this->showToolTip);
         echo "
