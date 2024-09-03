4c65cae8a44a4518b389ddd6c2d0cbeda2af70ab
Wayeet <kontakt@bjoern-roos.com>
2024-09-03 22:08:55 +0200

    Includer gets __DIR__ arg from user to stop exposing the absolute path to the dom

--- includer/Includer.php
+++ includer/Includer.php
@@ -4,17 +4,22 @@ namespace Util;
  * Can be used to import the correct versions of d3 and c3
  */
 class Includer {
-    private $selfDir = __DIR__ . '/../../vendor/wayeet/c3js-php';
-    public function __construct(){}
+    private $dir;
+    public function __construct(string $dir){
+        $this->dir = $dir;
+    }
 
+    private function getCWD(){
+        return $this->dir.'/vendor/wayeet/c3js-php';
+    }
     /**
      * Imports required CSS files
      * @return void This injects the imports directly to html
      */
     public function includeCSS(){
         echo '
-        <link href="'.$this->selfDir.'/c3-0.7.20/c3.css" rel="stylesheet">
-        <script src="'.$this->selfDir.'/d3v5/d3.min.js" charset="utf-8"></script>
+        <link href="'.$this->getCWD().'/c3-0.7.20/c3.css" rel="stylesheet">
+        <script src="'.$this->getCWD().'/d3v5/d3.min.js" charset="utf-8"></script>
         ';
     }
     /**
@@ -23,7 +28,7 @@ class Includer {
     */
     public function includeJS(){
         echo '
-        <script src="'.$this->selfDir.'/c3-0.7.20/c3.min.js"></script>
+        <script src="'.$this->getCWD().'/c3-0.7.20/c3.min.js"></script>
         ';
     }
     /**
